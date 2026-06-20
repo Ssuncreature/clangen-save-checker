b10a04631ddac28be9afae64542e699ff9ed3b54
Ssuncreature <sunniiflowo.dawn@gmail.com>
2026-06-20 20:12:34 +1200

    hopefully working json validator??

---
+++ scripts/clan.js
@@ -0,0 +1,18 @@
+function stableClan() {
+    try {
+    JSON.parse(text);
+    // Add the long if statement collection
+  } catch (e) {
+      // Do something if it fails
+      document.getElementById("clanresults").innerText = "Invalid JSON format for clan.json! You probs have an extra comma or bracket somewhere!";
+  }
+}
+function devClan() {
+    try {
+    JSON.parse(text);
+    // Add the long if statement collection
+  } catch (e) {
+      // Do something if it fails
+      document.getElementById("clanresults").innerText = "Invalid JSON format for clan.json! You probs have an extra comma or bracket somewhere!";
+  }
+}
---
+++ scripts/clancats.js
@@ -0,0 +1,18 @@
+function stableClanCats() {
+    try {
+    JSON.parse(text);
+    // Add the long if statement collection
+  } catch (e) {
+      // Do something if it fails
+      document.getElementById("catresults").innerText = "Invalid JSON format for clan_cats.json! You probs have an extra comma or bracket somewhere!";
+  }
+}
+function devClanCats() {
+    try {
+    JSON.parse(text);
+    // Add the long if statement collection
+  } catch (e) {
+      // Do something if it fails
+      document.getElementById("catresults").innerText = "Invalid JSON format for clan_cats.json! You probs have an extra comma or bracket somewhere!";
+  }
+}
