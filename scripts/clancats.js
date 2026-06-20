68f3ad5d8beaec950d60fc53fa4805251a77de43
Ssuncreature <sunniiflowo.dawn@gmail.com>
2026-06-20 20:29:46 +1200

    undo

--- scripts/clancats.js
+++ scripts/clancats.js
@@ -1,20 +1,6 @@
 function stableClanCats() {
     text = document.getElementById("catsstable").value;
-    try {
-    JSON.parse(text);
-    // Add the long if statement collection
-  } catch (e) {
-      // Do something if it fails
-      document.getElementById("catresults").innerText = "Invalid JSON format for clan_cats.json! You probs have an extra comma or bracket somewhere!";
-  }
 }
 function devClanCats() {
     text = document.getElementById("catsdev").value;
-    try {
-    JSON.parse(text);
-    // Add the long if statement collection
-  } catch (e) {
-      // Do something if it fails
-      document.getElementById("catresults").innerText = "Invalid JSON format for clan_cats.json! You probs have an extra comma or bracket somewhere!";
-  }
 }
