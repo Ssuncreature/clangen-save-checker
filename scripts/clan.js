9e0ac3090da71cfccd7bf3ed8290f1326d0cdde1
Ssuncreature <sunniiflowo.dawn@gmail.com>
2026-06-22 08:53:22 +1200

    initialise id comparison

--- scripts/clan.js
+++ scripts/clan.js
@@ -4,9 +4,4 @@ import "dev_checker.html";
 
 function stableClan() {
   var input = document.getElementById("clanstable").value;
-  if (input.includes("save_id")) {
-    
-  } else {
-    document.getElementById('clanresults').innerHTML = "somethings wrong with your save_id!"
-  }
 }
