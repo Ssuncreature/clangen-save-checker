274d2e494888f878ceed96d33a8486292fef9c12
Ssuncreature <sunniiflowo.dawn@gmail.com>
2026-06-22 15:10:31 +1200

    TEENSY bit of progress

--- scripts/id.js
+++ scripts/id.js
@@ -1,29 +1,16 @@
-function myFunction(){
-  var x = document.getElementById("myFile");
-  var txt = "";
-  if ('files' in x) {
+function IDcompare(){
+  var clan = document.getElementById("clan.json");
+  var clancats = document.getElementById("clan_cats");
+  if ('files' in clan) {
     if (x.files.length == 0) {
-      txt = "Select one or more files.";
+      txt = "Select your clan.json!";
     } else {
-      for (var i = 0; i < x.files.length; i++) {
-        txt += "<br><strong>" + (i+1) + ". file</strong><br>";
-        var file = x.files[i];
-        if ('name' in file) {
-          txt += "name: " + file.name + "<br>";
-        }
-        if ('size' in file) {
-          txt += "size: " + file.size + " bytes <br>";
-        }
+      
       }
     }
-  } 
   else {
     if (x.value == "") {
       txt += "Select one or more files.";
-    } else {
-      txt += "The files property is not supported by your browser!";
-      txt  += "<br>The path of the selected file: " + x.value; // If the browser does not support the files property, it will return the path of the selected file instead. 
     }
   }
-  document.getElementById("demo").innerHTML = txt;
 }
