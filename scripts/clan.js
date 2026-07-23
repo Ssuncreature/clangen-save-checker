2f0859562016556b5985dcd11edce273b1e03ddc
Ssuncreature <sunniiflowo.dawn@gmail.com>
2026-07-24 09:49:31 +1200

    add no file for stable clan checker

--- scripts/clan.js
+++ scripts/clan.js
@@ -3,5 +3,14 @@ import "stable_checker.html";
 import "dev_checker.html";
 
 function stableClan() {
-  var input = document.getElementById("clanstable").value;
+  event.preventDefault()
+  const clanInput = document.getElementById('stableclan.json')
+  // make sure there's actually info 
+  if (clanInput.files.length === 0) {
+  document.getElementById("clanresults").textContent="No clan.json uploaded!";
+  }
+  else {
+  const clan = JSON.parse(clanInput)
+
+  }
 }
