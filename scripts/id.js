02ef4866a3eb2503e9d30923998f27c01fd7f4ac
Ssuncreature <sunniiflowo.dawn@gmail.com>
2026-08-13 11:45:17 +1200

    trying to learn properties

--- scripts/id.js
+++ scripts/id.js
@@ -1,16 +1,15 @@
 function IDcompare(){
   event.preventDefault()
-  const clanInput = document.getElementById('clan.json')
-  const catInput = document.getElementById('clan_cats')
-  // make sure there's actually info 
-  if (clanInput.files.length === 0) {
-  document.getElementById("missingid").textContent="No clan.json uploaded!";
-  }
-  else if (catInput.files.length === 0) {
-  document.getElementById("missingid").textContent="No clan_cats.json uploaded!";
-  }
-  else {
-  const clan = JSON.parse(clanInput)
-  const cat = JSON.parse(catInput)
-  }
+  /**
+       * @typedef CatsID
+       * @type {object}
+       * @property {string} ID
+       */
+  /**
+   * @typedef ClanID
+   * @type {object}
+   * @property {string} IDs
+   */
+
+  
 }
