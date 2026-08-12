cadad2e1064421b62580f29997c8ca445be32f3d
Ssuncreature <sunniiflowo.dawn@gmail.com>
2026-08-13 11:50:07 +1200

    some schmol progress

--- scripts/id.js
+++ scripts/id.js
@@ -11,5 +11,15 @@ function IDcompare(){
    * @property {string} IDs
    */
 
-  
+    /** READ THE FILES GOES HERE */
+
+  const memberByID = {}
+    for (const member of clanCats) {
+      memberByID[member.ID] = member;
+    }
+  missing_cats = {}
+
+    if (member.ID (not in clan.clan_cats)) {
+      missing_cats.append(member.ID)
+    }
 }
